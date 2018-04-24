f59d71e23f3b36379e9f633fe4d074685a0ea92d
Krissy Conant <krissy@memberclicks.com>
2018-04-24 18:05:13 -0400

    tried to add modal...but you know

--- students/script.js
+++ students/script.js
@@ -5,8 +5,8 @@ $(function () {
     // get call the json file + connect above array with sort function + appending information
     $.get('https://s3.amazonaws.com/dc-profiles/Students.json', function(currentCard) {
         studentCardArray = currentCard.sort(function(a,b) {return 0.5 - Math.random()});
-        var test = renderCards(studentCardArray);
-        $('.mainBody').html(test);
+        var randomCard = renderCards(studentCardArray);
+        $('.mainBody').html(randomCard);
     });
 
     // physical rep rendering information to page 
@@ -18,9 +18,46 @@ $(function () {
             allCards += '<div class="card-body">';
             allCards += '<h4 class="card-text fullName">' + currentCard.fullName + '</h4>';
             allCards += '<p class="card-text missionStatement"> Motto: <br>' + currentCard.missionStatement + '</p>'; 
-            allCards += '<a href="' + currentCard.githubUrl + '" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">GitHub</a><br>'; 
-            allCards += '<a href="' + currentCard.portfolioUrl+ '" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Portfolio</a><br>'; 
-            allCards += '<a href="mailto:' + currentCard.email+ '" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Email</a><br>'; 
+            allCards += '<a href="' + currentCard.portfolioUrl + '" class="btn btn-primary btn-lg active " role="button" aria-pressed="true">Portfolio</a><br>'; 
+            allCards += '<a href="mailto:' + currentCard.email + '" class="btn btn-primary btn-lg active " role="button" aria-pressed="true">Email</a><br>'; 
+            allCards += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#"' + currentCard.id + 'Learn more about ' + currentCard.firstName + '</button>';
+
+            currentCard.showcase.forEach(function(show) {
+                allCards += '<p class="showcase">' + currentCard.fullBio +'</p>'
+                allCards += '<p class="showcase">' + currentCard.projectName +'</p>'
+                allCards += '<p class="showcase">' + currentCard.url +'</p>'
+                allCards += '<p class="showcase">' + currentCard.githubUrl +'</p>'
+                allCards += '<p class="showcase">' + currentCard.description +'</p>'
+                allCards += '<p class="showcase">' + currentCard.demoVideo +'</p>'
+                allCards += '<p class="showcase">' + currentCard.linkedinUrl +'</p>'
+            });
+            
+            
+
+/*Modal
+<div class="modal fade" id="' +  tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered" role="document">
+<div class="modal-content">
+    <div class="modal-header">
+    <h5 class="modal-title" id="exampleModalLongTitle"> + currentCard.firstName + </h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+    </div>
+    <div class="modal-body">
+    ...
+    </div>
+    <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    <button type="button" class="btn btn-primary">Contact Student</button>
+    </div>
+</div>
+</div>
+</div>
+*/
+
+
+            
             allCards += '</div>';
             allCards += '</div>';
         });
@@ -40,7 +77,6 @@ $(function () {
 });
 
 /*
-
 to-do list:
 make modal --> include full bio, linkedIn, showcase
 
@@ -63,33 +99,4 @@ showcase (this one is an array of github projects) - mod
 - url
 - description
 - demoVideo
-
---
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-Learn More! 
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-<div class="modal-dialog modal-dialog-centered" role="document">
-<div class="modal-content">
-    <div class="modal-header">
-    <h5 class="modal-title" id="exampleModalLongTitle"> + currentCard.firstName + </h5>
-    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-    </div>
-    <div class="modal-body">
-    ...
-    </div>
-    <div class="modal-footer">
-    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-primary">Contact Student</button>
-    </div>
-</div>
-</div>
-</div>
-
 */
